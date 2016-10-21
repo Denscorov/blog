89483bacfc036d08ecd3513f857de8b0cd18f270
Denis Skorov <wally.eva@yandex.ru>
2016-10-21 22:43:11 +0300

    init project and SDKBundle

--- app/app/AppKernel.php
+++ app/app/AppKernel.php
@@ -15,11 +15,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            
 
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-
-            new BlogBundle\BlogBundle(),
-            new UserBundle\UserBundle(),
+            new SDK\SDKBundle\SDKBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
